f9373ee88f3349f63ead09566c243d6a802775f2
Rahat-swe <onnokobi@gmail.com>
2020-04-11 22:26:42 +0600

    project upload

---
+++ madi_care/Controllers/DoctorController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using madi_care.Models;
+
+namespace madi_care.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            using (medi_careEntities2 db = new medi_careEntities2())
+            {
+                return View(db.admins.ToList());
+            }
+        }
+
+        // GET: Doctor/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Doctor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctor/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctor/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctor/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctor/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctor/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ madi_care/Controllers/adminsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using madi_care.Models;
+
+namespace madi_care.Controllers
+{
+    public class adminsController : Controller
+    {
+        private medi_careEntities2 db = new medi_careEntities2();
+
+        // GET: admins
+        public ActionResult Index()
+        {
+            var admins = db.admins.Include(a => a.admin1).Include(a => a.admin2);
+            return View(admins.ToList());
+        }
+
+        // GET: admins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: admins/Create
+        public ActionResult Create()
+        {
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname");
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname");
+            return View();
+        }
+
+        // POST: admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "aid,aname,aphone,aaddress,agender,aemail,apassword,abirth_date,aage")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            return View(admin);
+        }
+
+        // GET: admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            return View(admin);
+        }
+
+        // POST: admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "aid,aname,aphone,aaddress,agender,aemail,apassword,abirth_date,aage")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            ViewBag.aid = new SelectList(db.admins, "aid", "aname", admin.aid);
+            return View(admin);
+        }
+
+        // GET: admins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            admin admin = db.admins.Find(id);
+            db.admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
